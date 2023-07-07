fd94f3b0300de9fc9b1ebec3fb07792d970c86c5
Maiyo <58727496+maiyo008@users.noreply.github.com>
2023-07-07 19:49:38 +0300

    Update main.ts for task 6

--- 0x04-TypeScript/task_2/main.ts
+++ 0x04-TypeScript/task_2/main.ts
@@ -50,3 +50,21 @@ function createEmployee(salary: number | string): Director | Teacher {
 console.log(createEmployee(200)); // Teacher
 console.log(createEmployee(1000)); // Director
 console.log(createEmployee("$500")); // Director
+
+// Task 6
+function isDirector(employee: Director | Teacher): employee is Director {
+  return (employee as Director).workDirectorTasks !== undefined;
+}
+
+function executeWork(employee: Director | Teacher): void {
+  if (isDirector(employee)) {
+    console.log(employee.workDirectorTasks());
+  } else {
+    console.log(employee.workTeacherTasks());
+  }
+}
+
+// Example usage
+executeWork(createEmployee(200)); // Getting to work
+executeWork(createEmployee(1000)); // Getting to director tasks
+
