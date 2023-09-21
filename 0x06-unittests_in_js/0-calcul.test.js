32cf571d6e1347cfac45b67656741e8756ed79fe
maiyo008 <tonymaiyo008@gmail.com>
2023-09-21 14:45:06 +0300

    Adding more edge casses tests

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -2,10 +2,28 @@ const assert = require("assert");
 const calculateNumber = require("./0-calcul.js");
 
 describe("calculateNumber", function () {
-  it("should return the sum of two rounded numbers", function() {
+  it("sum of two whole numbers", function() {
     assert.strictEqual(calculateNumber(1,3), 4);
-    assert.strictEqual(calculateNumber(1,3.7), 5);
-    assert.strictEqual(calculateNumber(1.2,3.7), 5);
-    assert.strictEqual(calculateNumber(1.5,3.7), 6);
   });
-});
+  it("floating point numbers", function() {
+    assert.strictEqual(calculateNumber(1.0, 3.0), 4);
+  });
+  it("rounding off a backwards and add", function() {
+    assert.strictEqual(calculateNumber(1.223, 3), 4);
+  });
+  it("rounding off a forward and add", function() {
+    assert.strictEqual(calculateNumber(1.678, 3), 5);
+  });
+  it("rounding b back and adding", function() {
+    assert.strictEqual(calculateNumber(1, 3.333), 4);
+  });
+  it("rounding off b forward and adding", function () {
+    assert.strictEqual(calculateNumber(1, 3.764), 5);
+  });
+  it("rounding off foward both a and b and add them together", function () {
+    assert.strictEqual(calculateNumber(1.8956, 3.844), 6);
+  });
+  it("rounding down both a and b with trailing 9's", function () {
+    assert.equal(calculateNumber(1.4999, 3.49999), 4);
+  });
+  });
