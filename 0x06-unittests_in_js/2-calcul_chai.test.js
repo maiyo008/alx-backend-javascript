b879a843c257f518c6aed2c6f89f44a19cd1e789
maiyo008 <tonymaiyo008@gmail.com>
2023-09-21 15:57:37 +0300

    Basic test using Chai assertion library

---
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -0,0 +1,81 @@
+const chai = require("chai");
+const expect = chai.expect;
+const calculateNumber = require("./1-calcul.js");
+
+describe("calculateNumber(SUM)", function () {
+  it("sum of two whole numbers", function() {
+    expect(calculateNumber('SUM',1,3)).to.equal(4);
+  });
+  it("floating point numbers", function() {
+    expect(calculateNumber('SUM',1.0, 3.0)).to.equal(4);
+  });
+  it("rounding off a backwards and add", function() {
+    expect(calculateNumber('SUM',1.223, 3)).to.equal(4);
+  });
+  it("rounding off a forward and add", function() {
+    expect(calculateNumber('SUM',1.678, 3)).to.equal(5);
+  });
+  it("rounding b back and adding", function() {
+    expect(calculateNumber('SUM',1, 3.333)).to.equal(4);
+  });
+  it("rounding off b forward and adding", function () {
+    expect(calculateNumber('SUM',1, 3.764)).to.equal(5);
+  });
+  it("rounding off foward both a and b and add them together", function () {
+    expect(calculateNumber('SUM',1.8956, 3.844)).to.equal(6);
+  });
+  it("rounding down both a and b with trailing 9's", function () {
+    expect(calculateNumber('SUM',1.4999, 3.49999)).to.equal(4);
+  });
+  });
+
+  describe("calculateNumber(SUBTRACT)", function() {
+    it("Subtract two whole numbers when b>a", function() {
+        expect(calculateNumber('SUBTRACT',3, 1)).to.equal(2);
+    });
+    it("Subtracting whole numbers when a>b", function() {
+        expect(calculateNumber('SUBTRACT',1,3)).to.equal(-2);
+    });
+    it("rounding down a using trailing 9's", function() {
+        expect(calculateNumber('SUBTRACT',3.4999, 1)).to.equal(2);
+    });
+    it("rounding up a with trailing 0's", function() {
+        expect(calculateNumber('SUBTRACT',3.50000, 1)).to.equal(3);
+    });
+    it("rounding down b with trailing 9's", function() {
+        expect(calculateNumber('SUBTRACT',3, 1.499999)).to.equal(2);
+    });
+    it("rounding up b with trailing 0's", function() {
+        expect(calculateNumber('SUBTRACT',3, 1.50000)).to.equal(1);
+    });
+    it("rounding down both a and b", function() {
+        expect(calculateNumber('SUBTRACT',3.4999, 1.4999)).to.equal(2);
+    });
+    it("rounding up both a and b", function() {
+        expect(calculateNumber('SUBTRACT',3.5, 1.5)).to.equal(2);
+    });
+  });
+
+  describe("calculateNumber(DIVIDE)", function() {
+    it("When b=0", function() {
+        expect(calculateNumber('DIVIDE',4,0)).to.equal('Error');
+    });
+    it("rounding down a using trailing 9's", function() {
+        expect(calculateNumber('DIVIDE',3.4999, 1)).to.equal(3);
+    });
+    it("rounding up a with trailing 0's", function() {
+        expect(calculateNumber('DIVIDE',3.50000, 1)).to.equal(4);
+    });
+    it("rounding down b with trailing 9's", function() {
+        expect(calculateNumber('DIVIDE',4, 1.499999)).to.equal(4);
+    });
+    it("rounding up b with trailing 0's", function() {
+        expect(calculateNumber('DIVIDE',4, 1.50000)).to.equal(2);
+    });
+    it("rounding down both a and b", function() {
+        expect(calculateNumber('DIVIDE',3.4999, 1.4999)).to.equal(3);
+    });
+    it("rounding up both a and b", function() {
+        expect(calculateNumber('DIVIDE',3.5, 1.5)).to.equal(2);
+    });
+  });
